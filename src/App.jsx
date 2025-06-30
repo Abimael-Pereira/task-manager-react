4e0079ec48a6663d44897a47d8ae0a7f083eec79
Abimael <abimael.spdev@gmail.com>
2025-06-30 19:00:51 -0400

    feat: add Sidebar and SidebarButton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Sidebar from './components/Sidebar';
+
 function App() {
-  return <h1 className="font-bold">Hello world</h1>;
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
