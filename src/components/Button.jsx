c0d207b3bf7a6e23f93f05c5a6cac0fd20c5dffd
Abimael <abimael.spdev@gmail.com>
2025-06-30 22:40:44 -0400

    feat: add inicial Task and Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,22 @@
+const Button = ({ children, variant = 'primary' }) => {
+  const getVariantClasses = () => {
+    if (variant === 'primary') {
+      return 'bg-[#00ADB5] text-white';
+    }
+
+    if (variant === 'ghost') {
+      return 'bg-transparent text-[#818181]';
+    }
+
+    return 'text-[#35383E]';
+  };
+  return (
+    <button
+      className={`flex items-center gap-1 rounded-md px-3 py-1 text-xs font-semibold transition-opacity hover:opacity-80 ${getVariantClasses()}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
