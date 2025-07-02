fbfc3a065d565b924e540ef7aab7979f5050169d
Abimael <abimael.spdev@gmail.com>
2025-07-02 12:06:20 -0400

    style: add sort imports plugin

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,16 +1,17 @@
-import Button from './Button';
-import {
-  SunIcon,
-  CloudSunIcon,
-  MoonIcon,
-  TrashIcon,
-  AddIcon,
-} from '../assets/icons/';
-import TasksSeparator from './TasksSeparator';
-import TaskItem from './TaskItem';
-import TASKS from '../constants/tasks';
 import { useState } from 'react';
 import { toast } from 'sonner';
+
+import {
+  AddIcon,
+  CloudSunIcon,
+  MoonIcon,
+  SunIcon,
+  TrashIcon,
+} from '../assets/icons/';
+import TASKS from '../constants/tasks';
+import Button from './Button';
+import TaskItem from './TaskItem';
+import TasksSeparator from './TasksSeparator';
 
 const Tasks = () => {
   const [tasks, setTasks] = useState(TASKS);
