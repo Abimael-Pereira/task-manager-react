1550402af7434d45aad696f603c6ae8845c86c2e
Abimael <abimael.spdev@gmail.com>
2025-07-04 00:07:20 -0400

    refactor: fix the issues at TimeSelect component

--- src/components/TimeSelect.jsx
+++ src/components/TimeSelect.jsx
@@ -3,7 +3,7 @@ import { forwardRef } from 'react';
 import InputErrorMessage from './InputErrorMessage';
 import InputLabel from './InputLabel';
 
-const TimeSelect = forwardRef((props, ref) => {
+const TimeSelect = forwardRef(({ errorMessage, ...rest }, ref) => {
   return (
     <div className="flex flex-col text-left">
       <InputLabel htmlFor="time" label="Horário" />
@@ -11,19 +11,18 @@ const TimeSelect = forwardRef((props, ref) => {
       <select
         id="time"
         className="rounded-lg border border-solid border-brand-light-gray px-4 py-3 text-sm text-brand-dark-blue outline-brand-primary"
-        {...props}
+        {...rest}
         ref={ref}
+        defaultValue={'selecione'}
       >
-        <option value="" disabled selected>
+        <option value="selecione" disabled>
           Selecione
         </option>
         <option value="morning">Manhã</option>
         <option value="afternoon">Tarde</option>
         <option value="evening">Noite</option>
       </select>
-      {props.errorMessage && (
-        <InputErrorMessage>{props.errorMessage}</InputErrorMessage>
-      )}
+      {errorMessage && <InputErrorMessage>{errorMessage}</InputErrorMessage>}
     </div>
   );
 });
